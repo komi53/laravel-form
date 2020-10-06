f761183375f501106d226f69aeeb5d6a3221b866
komi53 <kxs53kuro@gmail.com>
2020-10-06 18:32:02 +0900

    route edit add

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -23,6 +23,7 @@ Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
     Route::get('create', 'ContactformController@create')->name('contact.create');
     Route::post('store', 'ContactformController@store')->name('contact.store');
     Route::get('show/{id}', 'ContactformController@show')->name('contact.show');
+    Route::get('edit/{id}', 'ContactformController@edit')->name('contact.edit');
 });
 
 
