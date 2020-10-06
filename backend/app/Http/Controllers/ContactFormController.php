e512ad468ef45670bda9b11077eb90653b1d6243
komi53 <kxs53kuro@gmail.com>
2020-10-06 18:34:04 +0900

    controller edit add

--- backend/app/Http/Controllers/ContactFormController.php
+++ backend/app/Http/Controllers/ContactFormController.php
@@ -116,6 +116,9 @@ class ContactFormController extends Controller
     public function edit($id)
     {
         //
+        $contact = ContactForm::find($id);
+
+        return view('contact.edit', compact('contact'));
     }
 
     /**
