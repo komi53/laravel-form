b60f9a89e6594de8463d372addcaceb2e2d5c015
komi53 <kxs53kuro@gmail.com>
2020-10-06 12:43:36 +0900

    show add

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -22,6 +22,7 @@ Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
     Route::get('index', 'ContactformController@index')->name('contact.index');
     Route::get('create', 'ContactformController@create')->name('contact.create');
     Route::post('store', 'ContactformController@store')->name('contact.store');
+    Route::get('show/{id}', 'ContactformController@show')->name('contact.show');
 });
 
 
